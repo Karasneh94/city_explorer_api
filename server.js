dd657c55eaf29a3d564df646db36127eb2664ef2
Ahmed Karasneh <karasneh94@gmail.com>
2021-04-05 14:55:20 +0300

    heroku test

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
+function Location (search_query, formatted_query, latitude, longitude) {
 
+    this.search_query = search_query;
+    this.formatted_query = formatted_query;
+    this.latitude = latitude;
+    this.longitude = longitude;
+}
 
 
 
@@ -13,20 +19,11 @@ const express = require('express');
 const cors = require('cors');
 const { request } = require('node:http');
 
-
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
 app.get('/location', handleLocation);
-
-function Location (search_query, formatted_query, latitude, longitude) {
-
-    this.search_query = search_query;
-    this.formatted_query = formatted_query;
-    this.latitude = latitude;
-    this.longitude = longitude;
-}
 
 app.get('/weather', handleWeather);
 
@@ -34,7 +31,9 @@ app.get('/weather', handleWeather);
 function handleLocation(request, response) {
 
     const getLocation = require('./data/location.json');
-    let locationObj = new Location(getLocation[0].city, getLocation[0].display_name, getLocation[0].lat, getLocation[0].lon);
+    const city = request.query.city;
+    console.log(city);
+    let locationObj = new Location(city, getLocation[0].display_name, getLocation[0].lat, getLocation[0].lon);
     response.send(locationObj);
 }
 
